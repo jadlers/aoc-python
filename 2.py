ea7e2410459d67fbbe4f9587b7839ccb2c9a1e92
Jacob Adlers <jacob.adlers@gmail.com>
2020-12-02 07:07:33 +0100

    Cleanup day2

--- 2.py
+++ 2.py
@@ -3,48 +3,30 @@
 import fileinput
 import re
 
-valid = 0
+# Example line:
+# 6-12 p: pfpppzpppzpbpg
 
 
-def p1():
-    p = re.compile("(\d+)-(\d+) ([a-z]): (\w+)")
-    xs = [x for x in fileinput.input()]
-    for x in xs:
-        res = p.match(x)
-        start = int(res.group(1))
-        end = int(res.group(2))
-        letter = res.group(3)
-        pw = res.group(4)
+p1 = 0
+p2 = 0
+p = re.compile("(\d+)-(\d+) ([a-z]): (\w+)")
+xs = [x for x in fileinput.input()]
 
-        searched = [x for x in pw if x == letter]
+for x in xs:
+    res = p.match(x)
+    start = int(res.group(1))
+    end = int(res.group(2))
+    letter = res.group(3)
+    pw = res.group(4)
 
-        if len(searched) >= start and len(searched) <= end:
-            valid += 1
-            print(x)
+    searched = [x for x in pw if x == letter]
 
+    if start <= len(searched) <= end:
+        p1 += 1
 
-def p2():
-    global valid
-    p = re.compile("(\d+)-(\d+) ([a-z]): (\w+)")
-    xs = [x for x in fileinput.input()]
-    for x in xs:
-        res = p.match(x)
-        start = int(res.group(1)) - 1
-        end = int(res.group(2)) - 1
-        letter = res.group(3)
-        pw = res.group(4)
-
-        searched = [x for x in pw if x == letter]
-        lval = 0
-        print(start, end, pw)
-        if pw[start] == letter:
-            lval += 1
-        if pw[end] == letter:
-            lval += 1
-
-        if lval == 1:
-            valid += 1
+    if (pw[start - 1] == letter) ^ (pw[end - 1] == letter):
+        p2 += 1
 
 
-p2()
-print(valid)
+print("part1: ", p1)
+print("part1: ", p2)
