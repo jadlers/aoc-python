abf45a5af5b5acf72a0265e0783de47c3b8d11b5
Jacob Adlers <jacob.adlers@gmail.com>
2020-12-06 07:03:30 +0100

    Finish 2020 day 6 part 2

--- 6.py
+++ 6.py
@@ -5,13 +5,26 @@ lines.append("")
 
 s = set()
 groups = []
+fline = True
 for line in lines:
     if line == "":  # end of group
         groups.append(s.copy())
         s = set()
+        fline = True
 
-    for ch in line:
-        s.add(ch)
+    else:
+        if fline:  # First round
+            fline = False
+            for ch in line:
+                s.add(ch)
+
+        else:
+            next = set()
+            for ch in line:
+                if ch in s:
+                    next.add(ch)
+
+            s = next.copy()
 
 p1 = 0
 for g in groups:
