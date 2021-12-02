876a1437c448e0a977c5c4980bb28bb9030e24ee
Jacob Adlers <jacob.adlers@gmail.com>
2021-12-02 12:56:28 +0100

    Finish 2021 D02P2
    Ugh, python is rusty!

--- 2021/2.py
+++ 2021/2.py
@@ -1,19 +1,36 @@
 import fileinput
 
-coll = {}
+coll = {
+    "aim": 0,
+    "forward": 0,
+    "down": 0,
+}
+
 
 lines = [x.strip() for x in fileinput.input()]
 for line in lines:
-    dir, dist = line.split()
+    dir, dist = line.split(" ")
+    dist = int(dist)
+
     if dir not in coll:
         coll[dir] = 0
+
+    if dir == "forward":
+        coll["forward"] += dist
+        coll["down"] += coll["aim"] * dist
+    elif dir == "down":
+        coll["aim"] += dist
+    elif dir == "up":
+        coll["aim"] -= dist
+
     print(dir, dist)
-    coll[dir] += int(dist)
+    print(coll)
+
 
 # commands = [(dir, int(dist) for (dir, dist) in lines]
 # lines = [[(y[0], y[1]) for y in x.strip().split()] for x in fileinput.input()]
 print(lines)
 print(coll)
 
-p1 = coll["forward"] * (coll["down"] - coll["up"])
-print(f"p1={p1}")
+p2 = coll["forward"] * (coll["down"] - coll["up"])
+print(f"p2={p2}")
