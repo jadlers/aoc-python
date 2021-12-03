7ee260fc7f9bf92764c97e49079f7f92aed64d42
Jacob Adlers <jacob.adlers@gmail.com>
2021-12-03 12:47:38 +0100

    Complete 2021 D03P2

--- 2021/3.py
+++ 2021/3.py
@@ -5,30 +5,41 @@ filename = sys.argv[1] if len(sys.argv) > 1 else "2.in"
 p1 = 0
 p2 = 0
 
-columns = {}
-for line in open(filename):
-    for (i, n) in enumerate(line.strip()):
-        if i not in columns:
-            columns[i] = []
-        columns[i].append(n)
+xs = [list(x.strip()) for x in open(filename)]
+
+def create_columns(xs):
+    cols = {}
+    for x in xs:
+        for (i, n) in enumerate(x):
+            if i not in cols:
+                cols[i] = {"list": []}
+            cols[i]["list"].append(n)
+    for col in cols.values():
+        col['0'] = col['list'].count('0')
+        col['1'] = col['list'].count('1')
+    return cols
+
+def oxy_rating(ys):
+    idx = 0
+    while len(ys) > 1:
+        cols = create_columns(ys)
+        common = '1' if cols[idx]['1'] >= cols[idx]['0'] else '0'
+        # print(idx, common)
+        ys = [y for y in ys if y[idx] == common]
+        idx = (idx + 1) % len(ys[0])
+        # print(ys)
+    return int("".join(ys[0]), base=2)
+
+def co2_rating(ys):
+    idx = 0
+    while len(ys) > 1:
+        cols = create_columns(ys)
+        common = '0' if cols[idx]['1'] >= cols[idx]['0'] else '1'
+        ys = [y for y in ys if y[idx] == common]
+        idx = (idx + 1) % len(ys[0])
+    return int("".join(ys[0]), base=2)
 
 
-vals = {}
-for i in columns:
-    ones = columns[i].count('1')
-    zero = columns[i].count('0')
+p2 = (oxy_rating(xs)) * (co2_rating(xs))
 
-    vals[i] = {
-        "gam": "1" if ones > zero else "0",
-        "eps": "1" if ones < zero else "0",
-    }
-
-gamma = "".join([vals[x]["gam"] for x in vals])
-epsilon = "".join([vals[x]["eps"] for x in vals])
-
-print(gamma, epsilon)
-
-p1 = int(gamma, base=2) * int(epsilon, base=2)
-
-print(f"p1={p1}")
 print(f"p2={p2}")
