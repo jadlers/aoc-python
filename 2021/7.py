5674a9d9987977647ecd2fc79457a750beb695a8
Jacob Adlers <jacob.adlers@gmail.com>
2021-12-07 06:40:27 +0100

    Improve 2021 D07

--- 2021/7.py
+++ 2021/7.py
@@ -2,19 +2,11 @@ import sys
 
 # From Jonathan Paulson
 filename = sys.argv[1] if len(sys.argv) > 1 else "2.in"
-p1 = 0
-p2 = 0
+p1 = []
+p2 = []
 
-C = {}
 def dist_cost(n):
-    if n in C:
-        return C[n]
-    res = 0
-    for i in range(n):
-        res += i+1
-
-    C[n] = res
-    return res
+    return n*(n+1)/2 # Compute instead of store
 
 xs = [int(x) for x in open(filename).readline().strip().split(",")]
 hi = max(xs)
@@ -22,16 +14,12 @@ hi = max(xs)
 dist = []
 for i in range(hi):
     i += 1
-    dist.append(sum([dist_cost(abs(j - i)) for j in xs]))
+    p1.append(sum([abs(j - i) for j in xs]))
+    p2.append(sum([dist_cost(abs(j - i)) for j in xs]))
 
 
-p1 = min(dist)
-
-# print(dist_cost(1))
-# print(dist_cost(2))
-# print(dist_cost(3))
-# print(dist_cost(4))
-
+p1 = min(p1)
+p2 = int(min(p2))
 
 # NOT 479665
 print(f"p1={p1}")
