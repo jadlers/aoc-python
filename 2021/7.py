07df5b569374eba46799ec073f4c463a2a3e7631
Jacob Adlers <jacob.adlers@gmail.com>
2021-12-07 06:14:14 +0100

    Finish 2021 D07P2

--- 2021/7.py
+++ 2021/7.py
@@ -5,6 +5,16 @@ filename = sys.argv[1] if len(sys.argv) > 1 else "2.in"
 p1 = 0
 p2 = 0
 
+C = {}
+def dist_cost(n):
+    if n in C:
+        return C[n]
+    res = 0
+    for i in range(n):
+        res += i+1
+
+    C[n] = res
+    return res
 
 xs = [int(x) for x in open(filename).readline().strip().split(",")]
 hi = max(xs)
@@ -12,10 +22,15 @@ hi = max(xs)
 dist = []
 for i in range(hi):
     i += 1
-    dist.append(sum([abs(j - i) for j in xs]))
+    dist.append(sum([dist_cost(abs(j - i)) for j in xs]))
 
 
 p1 = min(dist)
+
+# print(dist_cost(1))
+# print(dist_cost(2))
+# print(dist_cost(3))
+# print(dist_cost(4))
 
 
 # NOT 479665
