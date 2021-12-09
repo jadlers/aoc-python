ba1a6265a6dd6158925f4641d5e26b8c7eab7663
Jacob Adlers <jacob.adlers@gmail.com>
2021-12-09 16:31:19 +0100

    Finish 2021 D09P2

--- 2021/9.py
+++ 2021/9.py
@@ -2,11 +2,6 @@ import sys
 
 # From Jonathan Paulson
 filename = sys.argv[1] if len(sys.argv) > 1 else "2.in"
-p1 = 0
-p2 = 0
-
-# dx = [-1, 1]
-# dy = [-1, 1]
 
 lows = []
 rows = []  # rows[y][x]
@@ -32,9 +27,45 @@ def is_low(y, x):
 for y in range(len(rows)):
     for x in range(len(rows[0])):
         if is_low(y, x):
-            lows.append(rows[y][x])
+            lows.append((x, y))
 
-p1 = sum(lows) + len(lows)
+p1 = sum([rows[y][x] for (x,y) in lows]) + len(lows)
+
+# For P2 do a breadth first search-ish for the low points until edge/9
+def basin_size(x_init, y_init):
+    marked = {}
+    queue = [(x_init, y_init)]
+    size = 0
+    basin_coords = []
+    while len(queue) > 0:
+        [xc, yc] = queue[0]
+        queue = queue[1:]
+        size += 1  # count the location
+        basin_coords.append((xc, yc))
+        marked[(xc, yc)] = True
+
+        for dx, dy in [[-1, 0], [0, -1], [1, 0], [0, 1]]:
+            x = xc + dx
+            y = yc + dy
+            if (x < 0 or x >= len(rows[0])) or (y < 0 or y >= len(rows)):
+                continue
+
+            if (x, y) in marked:
+                continue
+            elif rows[y][x] != 9:
+                marked[(x, y)] = True
+                queue.append((x, y))
+    return size
+
+
+sizes = []
+for x, y in lows:
+    sizes.append(basin_size(x, y))
+
+# product of 3 biggest basin sizes
+a, b, c = sorted(sizes)[-3:]
+p2 = a * b * c
+
 
 print(f"p1={p1}")
 print(f"p2={p2}")
