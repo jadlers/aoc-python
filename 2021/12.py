9757a45d5d24166f1c31b02e45a0576b0edfc2d3
Jacob Adlers <jacob.adlers@gmail.com>
2021-12-12 12:40:17 +0100

    Clean up 2021 D12

--- 2021/12.py
+++ 2021/12.py
@@ -27,59 +27,54 @@ def add_edge(a, b, bidirectional=True):
         M[b].append(a)
 
 
-
 # Parse input
 for line in open(filename):
     s, e = line.strip().split("-")
     if s == "start" or e == "end":
-        add_edge(s,e,bidirectional=False)
+        add_edge(s, e, bidirectional=False)
     else:
-        add_edge(s,e)
+        add_edge(s, e)
 
-# Could remove all entries where a small cave only have connection to **one**
-# small cave
 
 def included_twice(s, visited):
     """Returns True if state `s` is included twice in `visited`, False otherwise"""
     try:
         idx = visited.index(s)
-        return visited[idx+1] == s
-    except(ValueError):
-        return False # Not included at all
-    except(IndexError):
-        return False # Last in list i.e. exists once
+        return visited[idx + 1] == s
+    except (ValueError):
+        return False  # Not included at all
+    except (IndexError):
+        return False  # Last in list i.e. exists once
 
 
-def step(paths):
+def new_path(path, s):
+    new = path.copy()
+    new.append(s)
+    return new
+
+
+def step(paths, p2: bool):
     """Take one step from each path in `paths` and return the new paths"""
     done = []
     next = []
     for path in paths:
-        visited = [x for x in path if x[0] in LOWER] # only visit small caves once
+        visited = [x for x in path if x[0] in LOWER]  # only visit small caves once
         visited.sort()
         has_revisit = False
-        for i,v in enumerate(visited):
-            if i == 0: continue
-            if visited[i-1] == v:
+        for i, v in enumerate(visited):
+            if i == 0:
+                continue
+            if visited[i - 1] == v:
                 has_revisit = True
                 break
 
-        # print(path, has_revisit, visited)
-
         for n in M[path[-1]]:
             # Add any state as long as there is no state revisited
-            if not has_revisit:
-                # print('can go to', n)
-                new = path.copy()
-                new.append(n)
-                if n == 'end':
-                    done.append(new)
-                else:
-                    next.append(new)
-            elif n not in visited:
-                new = path.copy()
-                new.append(n)
-                if n == 'end':
+            if (p2 and not has_revisit) or (
+                n not in visited
+            ):  # Don't include revisits for p1
+                new = new_path(path, n)
+                if n == "end":
                     done.append(new)
                 else:
                     next.append(new)
@@ -87,20 +82,18 @@ def step(paths):
     return done, next
 
 
-complete = []
-_, queue = step([['start']])
-last_len = -1
-# for _ in range(10):
-# while last_len < len(n):
-while len(queue) > 0:
-    done, queue = step(queue)
-    # print(len(done[0]), 'completed', done)
-    complete.extend(done)
+def run(p2):
+    complete = []
+    _, queue = step([["start"]], p2)
+    while len(queue) > 0:
+        done, queue = step(queue, p2)
+        complete.extend(done)
 
-# print(queue)
-# print(len(complete), complete)
-p1 = len(complete)
+    return len(complete)
 
+
+p1 = run(p2=False)
+p2 = run(p2=True)
 
 
 print(f"p1={p1}")
