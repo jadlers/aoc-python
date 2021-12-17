5534d79cca417f996bbe3e9778070883935f2ce4
Jacob Adlers <jacob.adlers@gmail.com>
2021-12-17 22:27:57 +0100

    WIP: Implemented using diagonals
    Correct for sample input

--- 2021/15.py
+++ 2021/15.py
@@ -15,33 +15,60 @@ for line in open(filename):
 
 R = len(rows)
 C = len(rows[0])
-print(R, C)
-
-least_cost = [[1e9 for _ in range(C)] for _ in range(R)]
-least_cost[0][0] = 0  # rows[0][0]
-
-# Idea: From the start position (0,0) take all possible steps and store the
-# lowest possible score to get there.
-Q = set()
-Q.add((0, 0))
-
-while Q:
-    r, c = Q.pop()
-    print("current", (r, c))
-    for i in range(2):
-        rr = r + DR[i]
-        cc = c + DC[i]
-        if 0 <= rr < R and 0 <= cc < C:
-            Q.add((rr, cc))
-            this_cost = least_cost[r][c] + rows[rr][cc]
-            if this_cost < least_cost[rr][cc]:
-                least_cost[rr][cc] = this_cost
 
 
-print(rows)
-print(least_cost)
-p1 = least_cost[-1][-1]
+def cost_to(costs, r, c):
+    if r < 0 or c < 0:
+        return int(1e9)
+    return costs[r][c]
 
+
+def calculate_least_cost(rows):
+    R = len(rows)
+    C = len(rows[0])
+    assert R == C
+
+    least_cost = [[1e9 for _ in range(C)] for _ in range(R)]
+    least_cost[0][0] = 0
+
+    # IDEA: Is the problem that an optimal route won't always go right/down.
+    # Will it actually at some point go up/left?
+    for i in range(1, R + C):  # Move in diagonals
+        for r in range(i + 1):
+            c = i - r
+            if r >= R or c >= C:
+                continue
+
+            ar, ac = r - 1, c  # Above
+            lr, lc = r, c - 1  # Left
+
+            above_cost = cost_to(least_cost, ar, ac)
+            left_cost = cost_to(least_cost, lr, lc)
+            least = above_cost if above_cost < left_cost else left_cost
+            least_cost[r][c] = least + rows[r][c]
+
+            # print((r, c), "above", (ar, ac), above_cost, "left:", (lr, lc), left_cost)
+            # print('least', least)
+
+    return least_cost[-1][-1]
+
+
+p1 = calculate_least_cost(rows)
+
+
+## Part 2
+B = []
+for tr in range(5):  # Tile row
+    tile = [[] for _ in range(R)]
+    for tc in range(5):  # Tile col
+        for r, row in enumerate(rows):
+            c = tr + tc
+            ncol = [x + c if x + c < 10 else x + c - 9 for x in row]
+            tile[r].extend(ncol)
+    for row in tile:
+        B.append(row)
+
+p2 = calculate_least_cost(B)
 
 print(f"p1={p1}")
-print(f"p2={p2}")
+print(f"p2={p2}")  # Not 2973 (too high)
