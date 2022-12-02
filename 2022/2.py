15e42677ec6750ac6e37b33bd8b44a9828afdafc
Jacob Adlers <jacob.adlers@gmail.com>
2022-12-02 06:57:25 +0100

    Clean up 2022 D02

--- 2022/2.py
+++ 2022/2.py
@@ -14,13 +14,17 @@ map = {
     "Z": 3,
 }
 
-name = {1: "rock", 2: "papr", 3: "scis"}
-
 for line in open(filename):
-    round = 0
     [op, me] = [x.strip() for x in line.strip().split(" ")]
-    play = map.get(me, 0)  # one you play
+    p1 += map.get(me, 0)  # one you play
 
+    diff = map[me] - map[op]
+    if diff == 0:
+        p1 += 3
+    elif (diff % 3) == 1:
+        p1 += 6
+
+    # P2
     chosen = -1
     if me == "X":  # lose
         chosen = map[op] - 1
@@ -32,22 +36,7 @@ for line in open(filename):
         p2 += 6
     if chosen < 1:
         chosen += 3
-        # print(map[op], chosen)
-
     p2 += chosen
-
-    diff = map[me] - map[op]
-    # print(name[map[op]], name[map[me]], diff)
-    outcome = 0
-    if diff == 0:
-        outcome = 3
-    elif diff == 1 or diff == -2:
-        outcome += 6
-
-    round += play
-    round += outcome
-    # print(f"{name[map[op]]} {name[map[me]]}, {play} + {outcome} ({diff}) = {round}")
-    p1 += round
 
 print(f"p1={p1}")  # 12231, 10893
 print(f"p2={p2}")  # 6459
