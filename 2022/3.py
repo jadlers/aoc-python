ce59297532668f92e088d7abe9a4780d374573b3
Jacob Adlers <jacob.adlers@gmail.com>
2022-12-03 06:51:12 +0100

    Improve 2022 D03
    Document and use set instead of ranging manually through to find
    intersection.

--- 2022/3.py
+++ 2022/3.py
@@ -12,12 +12,17 @@ assert len(letters) == 52
 
 # calculate value
 def dup_points(ch_set: set) -> int:
+    """calulate value of the letters in `ch_set`.
+
+    - Lowercase item types `a` through `z` have priorities 1 through 26.
+    - Uppercase item types `A` through `Z` have priorities 27 through 52.
+    """
     points = [letters.index(ch) + 1 for ch in "".join(ch_set)]
     return sum(points)
 
 
 last_group = -1
-intersec = ""
+common = set(letters)
 for (idx, line) in enumerate(X):
     # Part 1
     half = floor(len(line) / 2)
@@ -33,17 +38,15 @@ for (idx, line) in enumerate(X):
     # Part 2
     group = floor(idx / 3)
     if last_group != group and last_group >= 0:
-        p2 += letters.index(intersec[0]) + 1
-        intersec = ""
-    if intersec == "":
-        intersec = line
-    else:
-        intersec = [x for x in line if x in intersec]
+        assert len(common) == 1
+        p2 += letters.index(common.pop()) + 1
+        common = set(letters)
+    common &= set(line)
 
     last_group = group
 
 # Part 2
-p2 += letters.index(intersec[0]) + 1
+p2 += letters.index(common.pop()) + 1
 
 
 print(f"p1={p1}")  # 12231, 10893
