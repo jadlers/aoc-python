51f1887abf144e834c45765783b257a73f69bd05
Jacob Adlers <jacob.adlers@gmail.com>
2022-12-04 06:28:52 +0100

    Finish 2022 D04P2

--- 2022/4.py
+++ 2022/4.py
@@ -9,12 +9,13 @@ p2 = 0
 
 
 def pline(s, e):
-    return "".join(["x" if s<=i+1<=e else " " for i in range(e)])
+    return "".join(["x" if s <= i + 1 <= e else " " for i in range(e)])
+
 
 # for line in open(filename):
 for line in X:
     [[a, b], [x, y]] = [[int(y) for y in x.split("-")] for x in line.strip().split(",")]
-    
+
     # print("range", max(b,y)-min(a,x))
     # print("width", max(b,y))
     # print(a, b, x, y)
@@ -30,8 +31,13 @@ for line in X:
         # print("1", pline(a,b))
         # print("2", pline(x,y))
         # print('nope')
-
     # print()
+
+    # P2
+    fst = set([c for c in range(a, b + 1)])
+    snd = set([c for c in range(x, y + 1)])
+    if len(fst & snd) > 0:
+        p2 += 1
 
 
 print(f"p1={p1}")  # 512
