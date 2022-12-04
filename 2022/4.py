432dbab414e312a2940bacce8b07f9ab514a7403
Jacob Adlers <jacob.adlers@gmail.com>
2022-12-04 06:32:18 +0100

    Clean up 2022 D04

--- 2022/4.py
+++ 2022/4.py
@@ -3,35 +3,24 @@ import sys
 # From Jonathan Paulson
 filename = sys.argv[1] if len(sys.argv) > 1 else "4.in"
 X = [x.strip() for x in open(filename)]
-T = ["1-1,1-1"]
 p1 = 0
 p2 = 0
 
 
 def pline(s, e):
+    """Print a line where each char correspond to a section. The char is `"#"`
+    when the Elves shall clean it and `" "` otherwise."""
     return "".join(["x" if s <= i + 1 <= e else " " for i in range(e)])
 
 
-# for line in open(filename):
 for line in X:
     [[a, b], [x, y]] = [[int(y) for y in x.split("-")] for x in line.strip().split(",")]
 
-    # print("range", max(b,y)-min(a,x))
-    # print("width", max(b,y))
-    # print(a, b, x, y)
+    # P1
     if a <= x and b >= y:
-        # print(a, b, x, y)
         p1 += 1
     elif x <= a and y >= b:
-        # print(a, b, x, y)
         p1 += 1
-    else:
-        pass
-        # print(a,b,x,y)
-        # print("1", pline(a,b))
-        # print("2", pline(x,y))
-        # print('nope')
-    # print()
 
     # P2
     fst = set([c for c in range(a, b + 1)])
@@ -40,5 +29,5 @@ for line in X:
         p2 += 1
 
 
-print(f"p1={p1}")  # 512
-print(f"p2={p2}")  #
+print(f"p1={p1}")
+print(f"p2={p2}")
