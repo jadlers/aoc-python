8d67fd5b4855d392e87b874631e781a5584641b7
Jacob Adlers <jacob.adlers@gmail.com>
2022-12-05 06:39:49 +0100

    Clean up 2022 D05

--- 2022/5.py
+++ 2022/5.py
@@ -3,12 +3,12 @@ import re
 
 # From Jonathan Paulson
 filename = sys.argv[1] if len(sys.argv) > 1 else "5.in"
-# X = [x.strip() for x in open(filename)]
 p1 = 0
 p2 = 0
 
 
-stacks = [[] for _ in range(11)]  # same index as problem
+s1 = [[] for _ in range(11)]  # same index as in problem
+s2 = []
 setup = True
 move_re = re.compile(r"^move (\d+) from (\d+) to (\d+)$")
 
@@ -16,36 +16,36 @@ for line in open(filename):
     if line == "\n":
         continue
     if setup:
-        if line.startswith(" 1"):  # == "\n":
+        if line.startswith(" 1"):
             setup = False
-            for s in stacks:
+            for s in s1:
                 s.reverse()
-            print("setup done", stacks)
+            # print("setup done", s1)
+            s2 = [c.copy() for c in s1]
             continue
 
-        # print(line)
         for (idx, ch) in enumerate(line):
             if (idx) % 4 == 1 and ch != " ":
-                comp = (idx // 4) + 1  # comp as in problem
-                stacks[comp].append(ch)
+                # stack same as in problem
+                stack = (idx // 4) + 1
+                s1[stack].append(ch)
         continue
 
     # Moving stacks
     matches = move_re.match(line)
-    if not matches:
-        print(line)
-        continue
-    # assert matches != None
+    assert matches != None
     m, f, t = [int(x) for x in matches.groups()]
-    # print(m, f, t)
-    # print("move", stacks[f][-m:])
-    # for _ in range(m):
-    #     stacks[t].append(stacks[f].pop())
-    stacks[t].extend(stacks[f][-m:])
-    stacks[f] = stacks[f][:-m]
-    # print(stacks[1:4])
 
-p1 = "".join([c.pop() for c in stacks[1:] if len(c) > 0])
+    # P1
+    for _ in range(m):
+        s1[t].append(s1[f].pop())
+
+    # P2
+    s2[t].extend(s2[f][-m:])
+    s2[f] = s2[f][:-m]
+
+p1 = "".join([c.pop() for c in s1[1:] if len(c) > 0])
+p2 = "".join([c.pop() for c in s2[1:] if len(c) > 0])
 
 print(f"p1={p1}")
 print(f"p2={p2}")
