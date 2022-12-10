6553fec4cdd83aa11dc9153532169e9703eaae0f
Jacob Adlers <jacob.adlers@gmail.com>
2022-12-10 06:47:43 +0100

    Finish 2022 D10P2

--- 2022/10.py
+++ 2022/10.py
@@ -23,21 +23,50 @@ def inc_ss(strengths, cyc, x):
         return
 
 
+def screen(crt):
+    for row in crt:
+        print("".join(row))
+
+
+def draw(crt, cycle, pos):
+    cycle -= 1  # for index in crt to be correct
+    row = cycle // 40
+    col = cycle % 40
+    line = crt[row]
+    sprite = [pos - 1, pos, pos + 1]
+
+    # print(row, col, sprite, cycle in sprite)
+    if col in sprite:
+        line[col] = "x"
+
+    return row, line
+
+
 cycles = 0
 pos = 1
 ss = []
+crt = [["." for _ in range(40)] for _ in range(6)]
+
+
 for parts in X:
     cmd = parts[0]
     # print(cmd, parts)
     if cmd == "noop":
         cycles += M[cmd]
         inc_ss(ss, cycles, pos)
+        r, line = draw(crt, cycles, pos)
+        crt[r] = line
     elif cmd == "addx":
         for _ in range(M[cmd]):
             cycles += 1
+            r, line = draw(crt, cycles, pos)
+            crt[r] = line
             inc_ss(ss, cycles, pos)
         pos += int(parts[1])
+        # print("new pos", pos)
+
 
 p1 = sum(ss)
 print(f"p1={p1}")
-print(f"p2={p2}")
+print("\np2:") # PAPKFKEJ
+screen(crt)
