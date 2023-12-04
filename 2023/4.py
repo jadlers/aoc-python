0ffdb0bd1cf3d976bcba1de6261c88c673932dd1
Jacob Adlers <jacob.adlers@gmail.com>
2023-12-04 07:24:04 +0100

    2023 D4 clean up

--- 2023/4.py
+++ 2023/4.py
@@ -5,23 +5,27 @@ filename = sys.argv[1] if len(sys.argv) > 1 else "4.in"
 data = open(filename).read().strip()
 X = [x.strip() for x in data.split("\n")]
 
-p1, p2 = 0, 0
 
-
-num_cards = defaultdict(int)
+# Number of cards at key, note not using game number but line number.
+# line = game - 1.
+num_cards: defaultdict[int, int] = defaultdict(int)
 for i in range(len(X)):
     num_cards[i] = 1
-card_points = defaultdict(int)
+# Number of wins. Keyed just like `num_cards`
+card_wins: defaultdict[int, int] = defaultdict(int)
 
 
-def wins(game, winning, numbers):
-    if game in card_points:
-        return card_points[game]
+def wins(game: int, winning: list[int], numbers: list[int]):
+    """
+    Calculates the number of wins in a game. Also store the scoring for the
+    game in `num_cards`. 1p for the first win which is doubled for every other
+    win in the row.
+    """
     wins = 0
     for w in winning:
         if w in numbers:
             wins += 1
-    card_points[game] = 2 ** (wins - 1) if wins > 0 else 0
+    card_wins[game] = 2 ** (wins - 1) if wins > 0 else 0
     return wins
 
 
@@ -36,7 +40,7 @@ for i, line in enumerate(X):
             continue
         num_cards[g] += num_cards[i]
 
-p1 = sum(card_points.values())
+p1 = sum(card_wins.values())
 p2 = sum(num_cards.values())
 
 print(p1)
