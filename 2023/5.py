3b454a8703f4b1ed926b28134b5d31316483fab5
Jacob Adlers <jacob.adlers@gmail.com>
2023-12-05 07:40:46 +0100

    Finish 2023 D5P2
    Not proud of this one, took 17m50s execution time to find the answer.
    Will have to come up with some idea on how to optimize.

--- 2023/5.py
+++ 2023/5.py
@@ -5,12 +5,15 @@ filename = sys.argv[1] if len(sys.argv) > 1 else "5.in"
 data = open(filename).read().strip()
 X = [x.strip() for x in data.split("\n\n")]
 
-p1, p2 = inf, 0
+p1, p2 = inf, inf
 
 seeds = []
+available_seeds = []  # for P2
 names = []
 # NOTE: If missing in map, src = dst
 maps = []
+
+seed_loc = {}
 
 # Fill mappings
 for i, group in enumerate(X):
@@ -47,8 +50,60 @@ def find_location(seed):
     return seed
 
 
-for seed in seeds:
-    p1 = min(p1, find_location(seed))
+def has_seed(loc):
+    # print(f"has_seed({loc})")
+    for i in range(len(maps) - 1, -1, -1):
+        map = maps[i]
+        for r in map:
+            ds, de, ss, _ = r
+            # print(f"step {i}, ds={ds}, loc={loc}, de={de}, ss={ss}")
+            if ds <= loc <= de:
+                loc = ss + (loc - ds)
+                break
+
+    # Check to see if it's in available seeds
+    for seed_start, seed_end in available_seeds:
+        if seed_start <= loc < seed_end:
+            return loc
+
+    return -1
+
+
+for i in range(0, len(seeds), 2):
+    seed_start, length = seeds[i], seeds[i + 1]
+    available_seeds.append([seed_start, seed_start + length])
+
+loc = 0
+while True:
+    if loc % 100_000 == 0:
+        print("current location:", loc)
+    seed = has_seed(loc)
+    if seed != -1:
+        print(seed)
+        p2 = loc
+        break
+    loc += 1
+
+
+# NOTE: Going through seeds, way too slow
+# for i in range(0, len(seeds), 2):
+#     print(f"{i/2}/{len(seeds)/2}")
+#     seed_start, length = seeds[i], seeds[i + 1]
+#     for seed in range(seed_start, seed_start + length):
+#         if seed % 1_000_000 == 0:
+#             print(f"\t{seed}/{seed_start+length}")
+#         if seed in seed_loc:
+#             continue
+#         loc = find_location(seed)
+#         seed_loc[seed] = loc
+#         p2 = min(p2, loc)
+
+# Do we need to go backwards? Start from location and work our way to the seed?
+
+
+# P1
+# for seed in seeds:
+#     p1 = min(p1, find_location(seed))
 
 
 print(p1)
