53c37b2d86a1c94a263badf500cf3c84093743c7
Jacob Adlers <jacob.adlers@gmail.com>
2023-12-09 23:59:44 +0100

    Finish 2023 D8P2

--- 2023/8.py
+++ 2023/8.py
@@ -1,5 +1,6 @@
 import sys
 import re
+from math import gcd
 
 filename = sys.argv[1] if len(sys.argv) > 1 else "8.in"
 data = open(filename).read().strip()
@@ -9,25 +10,50 @@ iss = [x.split()[0] for x in X[0]]
 p1, p2 = 0, 0
 
 map = {}
+S = []
 r = re.compile(r"(\w{3}) = \((\w{3}), (\w{3})\)")
 for i, line in enumerate(X[2:]):
     m = r.match(line)
     assert m != None
     groups = m.groups()
     node, left, right = groups
-    # print(line, node, left, right)
     map[node] = [left, right]
+    if node[2] == "A":  # Start nodes in part 2
+        S.append(node)
 
 
-n = "AAA"
+# Part 1
+pos = "AAA"
 p = 0
 while True:
     i = iss[p]
     p = (p + 1) % len(iss)
     p1 += 1
-    n = map[n][0 if i == "L" else 1]
-    if n == "ZZZ":
+    pos = map[pos][0 if i == "L" else 1]
+    if pos == "ZZZ":
         break
+
+# Part 2
+# Find cycle length of all starting positions
+dists = []
+for pos in S:
+    steps = 0
+    p = 0
+    while True:
+        steps += 1
+        i = iss[p]
+        p = (p + 1) % len(iss)
+        pos = map[pos][0 if i == "L" else 1]
+        if pos[2] == "Z":
+            dists.append(steps)
+            break
+
+# Calculate the lowest common multiple
+# https://stackoverflow.com/a/42472824
+lcm = 1
+for i in dists:
+    lcm = lcm * i // gcd(lcm, i)
+p2 = lcm
 
 
 print(p1)
